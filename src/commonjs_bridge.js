4893313eaff83b5b3de44e8998693a00fc0e199d
Tim Hemming <timhemming@gmail.com>
2014-05-12 23:32:13 +0100

    mocks can be passed in to require() statements. When mocks are defined the module cache will be bypassed. Mocks are passed through to all sub requests, this results in creating a kind of scope for all require() calls.

--- src/commonjs_bridge.js
+++ src/commonjs_bridge.js
@@ -5,30 +5,57 @@ for (var modulePath in window.__cjs_module__) {
     require(modulePath, modulePath);
 };
 
-function require(requiringFile, dependency) {
+function normalizeMockKeys(parent, mocks, modulesRoot) {
+    var normalizedMocks = {};
+    for(var relativePath in mocks) {
+        if(mocks.hasOwnProperty(relativePath)) {
+            var normalizedPath = normalizePath(parent, relativePath, modulesRoot);
+            normalizedMocks[normalizedPath] = mocks[relativePath];
+        }
+    }
+    return normalizedMocks;
+}
+
+function require(requiringFile, dependency, mocks) {
 
     if (window.__cjs_module__ === undefined) throw new Error("Could not find any modules. Did you remember to set 'preprocessors' in your Karma config?");
     if (window.__cjs_modules_root__ === undefined) throw new Error("Could not find CommonJS module root path. Please report this issue to the karma-commonjs project.");
 
     dependency = normalizePath(requiringFile, dependency, window.__cjs_modules_root__ || '');
 
+    // return mocked dependency if one is declared
+    if(mocks && mocks[dependency] !== undefined) {
+        return mocks[dependency];
+    }
+
     // find module
     var moduleFn = window.__cjs_module__[dependency];
     if (moduleFn === undefined) throw new Error("Could not find module '" + dependency + "' from '" + requiringFile + "'");
 
     // run the module (if necessary)
-    var module = cachedModules[dependency];
+    var module;
+    if(!mocks) {
+        module = cachedModules[dependency];
+    }
+
     if (module === undefined) {
         module = { exports: {} };
-        cachedModules[dependency] = module;
-        moduleFn(requireFn(dependency), module, module.exports);
+        if(!mocks) {
+            cachedModules[dependency] = module;
+        }
+        moduleFn(requireFn(dependency, mocks), module, module.exports);
     }
+
     return module.exports;
 };
 
-function requireFn(basepath) {
-    return function(dependency) {
-        return require(basepath, dependency);
+function requireFn(basepath, bubbledMocks) {
+    return function(dependency, mocks) {
+        var fullPathMocks;
+        if(mocks) {
+            fullPathMocks = normalizeMockKeys(basepath, mocks, window.__cjs_modules_root__ || '');
+        }
+        return require(basepath, dependency, fullPathMocks || bubbledMocks);
     };
 };
 
@@ -43,7 +70,7 @@ function normalizePath(basePath, relativePath, modulesRoot) {
     var baseComponents = basePath.split("/");
     var relativeComponents = relativePath.split("/");
     var nextComponent;
-    
+
     if (!isNpmModulePath(relativePath)) {
         baseComponents.pop();     // remove file portion of basePath before starting
     }
@@ -54,7 +81,7 @@ function normalizePath(basePath, relativePath, modulesRoot) {
         else if (nextComponent === "..") baseComponents.pop();
         else baseComponents.push(nextComponent);
     }
-    
+
     var normalizedPath = baseComponents.join("/");
 
     if (normalizedPath.substr(normalizedPath.length - 3) !== ".js") {
