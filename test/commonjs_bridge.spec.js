e46beb977bf9cbc98d3f8b684ca38bf37a9caae6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-05-17 13:15:58 +0200

    test: add require high-level tests

--- test/commonjs_bridge.spec.js
+++ test/commonjs_bridge.spec.js
@@ -1,31 +1,79 @@
 describe('client', function() {
 
-	beforeEach(function(){
-		window.__cjs_module__ = {};
-		window.__cjs_modules_root__ = '/root';
-	});
+  describe('require high level tests', function () {
 
-	it('should correctly resolve modules with circular dependencies issue #6', function(){
+    beforeEach(function(){
+      window.__cjs_module__ = {};
+      window.__cjs_modules_root__ = '/root';
+    });
 
-		 window.__cjs_module__['/foo.js'] = function(require, module, exports) {
-		 	var bar = require('./bar');
-		 	exports.txt = 'foo';
-		 	exports.getText = function() {
-		 		return 'foo ' + bar.txt;
-		 	};		 	
-		 };
+    describe('resolve from a file', function () {
 
-		 window.__cjs_module__['/bar.js'] = function(require, module, exports) {
-		 	var foo = require('./foo');
-		 	exports.txt = 'bar';
-		 	exports.getText = function() {
-		 		return 'bar ' + foo.txt;
-		 	}
-		 };		
+      beforeEach(function () {
+        window.__cjs_module__['/folder/foo.js'] = function(require, module, exports) {
+          exports.foo = true;
+        };
+      });
 
-		expect(require('/app.js', './foo').getText()).toEqual('foo bar');
-		expect(require('/app.js', './bar').getText()).toEqual('bar foo');
-	});
+      it('should resolve absolute dependencies', function () {
+        expect(require('/folder/bar.js', '/folder/foo.js').foo).toBeTruthy();
+        //TODO: check how this works in node: expect(require('/folder/bar.js', '/folder/foo').foo).toBeTruthy();
+      });
+
+      it('should resolve relative dependencies', function () {
+        expect(require('/folder/bar.js', './foo').foo).toBeTruthy();
+        expect(require('/folder/bar.js', './foo.js').foo).toBeTruthy();
+        expect(require('/folder/bar.js', './../folder/foo.js').foo).toBeTruthy();
+      });
+    });
+
+    describe('resolve from node_modules', function () {
+
+      beforeEach(function () {
+        window.__cjs_modules_root__ = '/root/node_modules';
+        window.__cjs_module__['/root/node_modules/mymodule/foo.js'] = function(require, module, exports) {
+          exports.foo = true;
+        };
+      });
+
+      it('should resolve node_modules dependencies', function () {
+        expect(require('/folder/bar.js', 'mymodule/foo').foo).toBeTruthy();
+        expect(require('/folder/bar.js', 'mymodule/foo.js').foo).toBeTruthy();
+      });
+
+    });
+
+    describe('resolve - corner cases', function () {
+
+      it('should throw error when a required module does not exist', function () {
+        expect(function () {
+          require('/file.js', './bar')
+        }).toThrow(new Error("Could not find module '/bar.js' from '/file.js'"));
+      });
+
+      it('should correctly resolve modules with circular dependencies issue #6', function(){
+
+        window.__cjs_module__['/foo.js'] = function(require, module, exports) {
+          var bar = require('./bar');
+          exports.txt = 'foo';
+          exports.getText = function() {
+            return 'foo ' + bar.txt;
+          };
+        };
+
+        window.__cjs_module__['/bar.js'] = function(require, module, exports) {
+          var foo = require('./foo');
+          exports.txt = 'bar';
+          exports.getText = function() {
+            return 'bar ' + foo.txt;
+          }
+        };
+
+        expect(require('/app.js', './foo').getText()).toEqual('foo bar');
+        expect(require('/app.js', './bar').getText()).toEqual('bar foo');
+      });
+    });
+  });
 
 	describe('path resolving and normalization', function(){
 
